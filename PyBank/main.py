67229df1ebfcb73f8cf5dbe6346db558a2a6eb7b
Meg Sharma <megsharma@Megs-Air-2.attlocal.net>
2019-06-12 14:49:08 -0400

    Updated PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,2 +1,44 @@
-var newVariable = 5
-var secondVariable = "New"
+import os
+import csv
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+
+dates = []
+total = []
+change = []
+
+    
+
+
+with open(csvpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    if csv.Sniffer().has_header:
+        next(csvreader)
+    for row in csvreader: 
+        total.append(float(row[1]))
+        dates.append(row[0])
+    for i in range(1, len(total)): 
+        change.append(total[i] - total[i-1])
+        avg_change = sum(change) / len(change)
+        max_change = max(change)
+        min_change = min(change)
+        max_date_change = str(dates[change.index(max_change)])
+        min_date_change = str(dates[change.index(min_change)])
+
+print("Financial Analysis")
+print("-------------------------------")
+print(f'Total Months: {len(dates)}')
+print(f'Total: ${int(sum(total))}')
+print(f'Average Change: ${round(avg_change, 2)}')
+print(f'Greatest Increase in Profits: {max_date_change}  (${int(max_change)})')
+print(f'Greatest Decrease in Profits: {min_date_change}  (${int(min_change)})')
+
+
+f = open("output.txt", "a")
+print("Financial Analysis", file=f)
+print("-------------------------------", file=f)
+print(f'Total Months: {len(dates)}', file=f)
+print(f'Total: ${int(sum(total))}', file=f)
+print(f'Average Change: ${round(avg_change, 2)}', file=f)
+print(f'Greatest Increase in Profits: {max_date_change}  (${int(max_change)})', file=f)
+print(f'Greatest Decrease in Profits: {min_date_change}  (${int(min_change)})', file=f)
+f.close()
